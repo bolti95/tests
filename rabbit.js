ba8689de943f6b4720bcf742eb8d170dc1f55646
Isabelle Bolt <izzybolt95@gmail.com>
2020-09-25 12:11:59 +0100

    added array to test

--- rabbit.js
+++ rabbit.js
@@ -1,11 +1,16 @@
 const favouriteNumber = 4;
 
+
 class Rabbit {
-    constructor(name, health, hunger) {
+    constructor(name, health, hunger, message) {
         this.name = name;
         this.health = health;
         this.hunger = hunger;
         this.strength = 25;
+        this.message = "name added to Rabbit";
+        this.rabbitFood = ["carrot", "lettuce", "broccoli"]
+        
+   
     }
     increaseHealth() {
         this.health++;
@@ -16,12 +21,23 @@ class Rabbit {
     train() {
         this.strength += 5;
     }
+    addFood() {
+        let newFood = "celery"
+        this.rabbitFood.push(newFood);
+     
+    }
+
 }
 
 const freddy = new Rabbit("Freddy", 100, 100)
 console.log(freddy.name);
-freddy.decreaseHunger()
+freddy.decreaseHunger();
+
+freddy.addFood();
+console.log(freddy.rabbitFood)
+console.log(freddy.message)
 console.log(freddy.hunger)
+
 
 
 module.exports = {
--- tests/rabbit.test.js
+++ tests/rabbit.test.js
@@ -11,3 +11,11 @@ test("should show a decrease in hunger when decreaseHunger is called", () => {
     expect(app.freddy.hunger).toBe(98);
 
 })
+
+test("should log name message", () => {
+    expect(app.freddy.message).toEqual("name added to Rabbit");
+})
+
+test("should add new food to rabbitFood", () => {
+    expect(app.freddy.rabbitFood).toContain("celery");
+})
